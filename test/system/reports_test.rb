a3d97c5dcb94fe070a14c8e66c63fe21ec583018
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-25 16:58:37 +0900

    First system_test for Report

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -1,9 +1,19 @@
+# frozen_string_literal: true
+
 require "application_system_test_case"
 
 class ReportsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit reports_url
-  #
-  #   assert_selector "h1", text: "Reports"
-  # end
+  include Warden::Test::Helpers
+
+  setup do
+    @user = users(:one)
+  end
+
+  test 'visiting the index' do
+    login_as(@user, scope: :user)
+
+    visit reports_url
+
+    assert_selector 'h1', text: Report.model_name.human
+  end
 end
