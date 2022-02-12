82bab4cc3a76bca42c4dc424998b77539136ba9a
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-02-13 07:47:46 +0900

    Initiate devise by instructions of rails g devise:install

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
