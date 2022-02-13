de7f48048025d8d385e58e91f390d48cceedd0f2
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-02-13 11:41:18 +0900

    Update routes to use resources helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   devise_for :users
   resources :books
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "books#index"
