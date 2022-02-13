34fbb60cb412e02f84ed6a32b7a31356e71e304d
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-02-13 09:43:08 +0900

    Add logout link to each views

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "book#index"
+  root to: "books#index"
 end
