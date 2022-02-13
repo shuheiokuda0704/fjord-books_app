7799573305565ff0f5ef8ea0e32e75807297e0fc
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-02-13 13:41:47 +0900

    Revert "Generate custom controller for registrations"
    This reverts commit 9efbbf5e0eea0ad5226e0d99e2bcfc40bb49b113.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-
+  devise_for :users
   resources :books
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to: "books#index"
 end
