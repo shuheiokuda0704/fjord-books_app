87827d36e50b9e9092622b231e1420a83069846f
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-02-13 15:08:55 +0900

    Configure for letter_opener

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,5 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
