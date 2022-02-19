4f680c50d34dd53643e37a5e16ea3f8af4968d31
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-02-20 04:46:10 +0900

    [WIP] Add handling avatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :normal, resize: "300x300"
+    attachable.variant :thumbnail, resize: "100x100"
+  end
 end
