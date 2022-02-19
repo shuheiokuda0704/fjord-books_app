0ee0e66efa1e1cb9c58f8ca780947717fefc3502
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-02-20 04:48:41 +0900

    Move authenticate_user! to application_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @users = User.order(:id).page(params[:page])
   end
