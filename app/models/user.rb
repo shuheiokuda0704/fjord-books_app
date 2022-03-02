42e13869c22052f112a71d96a6cacdf49b1d254a
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-03 00:10:45 +0900

    Fix association name to adjust with requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :followings, dependent: :destroy
-  has_many :followers, class_name: :Following, foreign_key: :following_user_id, inverse_of: :following_user, dependent: :destroy
+  has_many :following_relations, class_name: :Following, dependent: :destroy
+  has_many :follower_relations, class_name: :Following, foreign_key: :following_user_id, inverse_of: :following_user, dependent: :destroy
 
-  has_many :following_users, through: :followings, source: :following_user
-  has_many :follower_users, through: :followers, source: :user
+  has_many :followings, through: :following_relations, source: :following_user
+  has_many :followers, through: :follower_relations, source: :user
 
   has_one_attached :avatar
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,18 +13,18 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'has_many: :followings association' do
-    assert_equal [@following_one_two.id, @following_one_three.id].sort, @user_one.followings.map(&:id).sort
+    assert_equal [@following_one_two.id, @following_one_three.id].sort, @user_one.following_relations.map(&:id).sort
   end
 
   test 'has_many: :followers association' do
-    assert_equal [@following_two_three.id, @following_one_three.id].sort, @user_three.followers.map(&:id).sort
+    assert_equal [@following_two_three.id, @following_one_three.id].sort, @user_three.follower_relations.map(&:id).sort
   end
 
   test 'has_many: :following_users association' do
-    assert_equal [@user_two.id, @user_three.id].sort, @user_one.following_users.map(&:id).sort
+    assert_equal [@user_two.id, @user_three.id].sort, @user_one.followings.map(&:id).sort
   end
 
   test 'has_many: :follower_users association' do
-    assert_equal [@user_one.id, @user_two.id].sort, @user_three.follower_users.map(&:id).sort
+    assert_equal [@user_one.id, @user_two.id].sort, @user_three.followers.map(&:id).sort
   end
 end
