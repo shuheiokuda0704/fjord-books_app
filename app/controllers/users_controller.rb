171506467ca88d07a1e0f8281fb9ae984aaf31ea
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-04 10:43:49 +0900

    Add followers/followings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,28 @@
 
 class UsersController < ApplicationController
   def index
+    @title = User.model_name.human
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
     @follow = Following.find_by(user: current_user, following_user: @user)
+  end
+
+  def followings
+    @title = 'フォロー中'
+    @user = User.find(params[:id])
+    @users = @user.followings.with_attached_avatar.order(:id).page(params[:page])
+
+    render 'index'
+  end
+
+  def followers
+    @title = 'フォロワー'
+    @user = User.find(params[:id])
+    @users = @user.followers.with_attached_avatar.order(:id).page(params[:page])
+
+    render 'index'
   end
 end
