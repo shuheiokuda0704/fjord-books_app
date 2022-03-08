17d09b25f407891deaa4edd9bc9adeced765f784
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-08 19:43:05 +0900

    Delete json support

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -3,24 +3,19 @@
 class FollowingsController < ApplicationController
   def create
     @following = Following.new(user: current_user, following_user_id: following_params[:following_user_id])
-    respond_to do |format|
-      if @following.save
-        format.html { redirect_to user_path(id: @following.following_user_id), notice: t('controllers.followings.notice_create') }
-        format.json { render 'users/show', status: :created, location: @following.following_user }
-      else
-        format.html { redirect_to user_path(id: @following.following_user_id) }
-        format.json { render json: @following.errors, status: :unprocessable_entity }
-      end
+
+    if @following.save
+      redirect_to user_path(id: @following.following_user_id), notice: t('controllers.followings.notice_create')
+    else
+      redirect_to user_path(id: @following.following_user_id)
     end
   end
 
   def destroy
     @following = Following.find(params[:id])
     @following.destroy
-    respond_to do |format|
-      format.html { redirect_to user_path(@following.following_user_id), notice: t('controllers.followings.notice_destroy') }
-      format.json { head :no_content }
-    end
+
+    redirect_to user_path(@following.following_user_id), notice: t('controllers.followings.notice_destroy')
   end
 
   def following_params
