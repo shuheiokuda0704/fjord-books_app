ab0ea6571b7f0e656ab490a058f7a80b8bf4c16f
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-14 22:24:41 +0900

    Accept to edit/update/destroy user own report only

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,25 +1,20 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[ show edit update destroy ]
-
-  # GET /reports or /reports.json
+  before_action :user_own_report?, only: %i[edit update destroy]
   def index
     @reports = Report.order(:created_at).page(params[:page])
   end
 
-  # GET /reports/1 or /reports/1.json
   def show
   end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
   def edit
   end
 
-  # POST /reports or /reports.json
   def create
     @report = Report.new(report_params)
 
@@ -30,32 +25,27 @@ class ReportsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: "Report was successfully updated." }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to @report, notice: "Report was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: "Report was successfully destroyed."
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_report
       @report = Report.find(params[:id])
+    end
+
+    def user_own_report?
+      redirect_to report_url unless @report.user.eql?(current_user)
     end
 
     # Only allow a list of trusted parameters through.
