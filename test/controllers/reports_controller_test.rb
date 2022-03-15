01047549e2e5b95fd24cbb184b6768dfc6700cc4
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-15 09:54:55 +0900

    Add test for report model & controller

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,48 +1,109 @@
 require "test_helper"
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
+
   setup do
-    @report = reports(:one)
+    @user_one = users(:one)
+    @user_two = users(:two)
+    @report_one = reports(:one)
+    @report_two = reports(:two)
+  end
+
+  test "should redirect to login_page if not lgged in" do
+    get reports_url
+
+    assert_response :redirect
+    assert_redirected_to new_user_session_url
   end
 
   test "should get index" do
+    login_as(@user_one, scope: :user)
+
     get reports_url
     assert_response :success
   end
 
   test "should get new" do
+    login_as(@user_one, scope: :user)
+
     get new_report_url
     assert_response :success
   end
 
   test "should create report" do
+    login_as(@user_one, scope: :user)
+
     assert_difference('Report.count') do
-      post reports_url, params: { report: { content: @report.content, title: @report.title } }
+      post reports_url, params: { report: { content: @report_one.content, title: @report_one.title } }
     end
 
     assert_redirected_to report_url(Report.last)
   end
 
   test "should show report" do
-    get report_url(@report)
+    login_as(@user_one, scope: :user)
+
+    get report_url(@report_one)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_report_url(@report)
+    login_as(@user_one, scope: :user)
+
+    get edit_report_url(@report_one)
     assert_response :success
   end
 
+  test "should not get edit for other user's report" do
+    login_as(@user_two, scope: :user)
+
+    get edit_report_url(@report_one)
+
+    assert_response :redirect
+    assert_redirected_to report_url(@report_one)
+  end
+
   test "should update report" do
-    patch report_url(@report), params: { report: { content: @report.content, title: @report.title } }
-    assert_redirected_to report_url(@report)
+    login_as(@user_one, scope: :user)
+
+    assert_no_difference('Report.count') do
+      patch report_url(@report_one), params: { report: { content: @report_one.content, title: @report_one.title } }
+    end
+
+    # TODO: コンテンツが更新されたことを確認
+    assert_redirected_to report_url(@report_one)
+  end
+
+  test "should not update report for other user's report" do
+    login_as(@user_two, scope: :user)
+
+    assert_no_difference('Report.count') do
+      patch report_url(@report_one), params: { report: { content: @report_one.content + "X", title: @report_one.title + "X"} }
+    end
+
+    # TODO: コンテンツが更新されていないことを確認
+    assert_response :redirect
+    assert_redirected_to report_url(@report_one)
   end
 
   test "should destroy report" do
+    login_as(@user_one, scope: :user)
+
     assert_difference('Report.count', -1) do
-      delete report_url(@report)
+      delete report_url(@report_one)
     end
 
     assert_redirected_to reports_url
+  end
+
+  test "should not destroy other user's report" do
+    login_as(@user_two, scope: :user)
+
+    assert_no_difference('Report.count') do
+      delete report_url(@report_one)
+    end
+
+    assert_redirected_to report_url(@report_one)
   end
 end
--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report = reports(:one)
+    @user = users(:one)
+  end
+
+  test "report has title and content" do
+    assert_equal "title-one", @report.title
+    assert_equal "content-one", @report.content
+  end
+  test "report belongs to user" do
+    assert_equal @user, @report.user
+  end
 end
