d3186365c89dfafb28852641b8f6f2e65b3b24a2
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-15 15:00:41 +0900

    Add create / update / destroy feature for report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[ show edit update destroy ]
+  before_action :set_report, only: %i[show edit update destroy]
   before_action :user_own_report?, only: %i[edit update destroy]
+
   def index
     @reports = Report.order(:created_at).page(params[:page])
   end
@@ -16,10 +17,10 @@ class ReportsController < ApplicationController
   end
 
   def create
-    @report = Report.new(report_params)
+    @report = current_user.reports.new(report_params)
 
     if @report.save
-      redirect_to @report, notice: "Report was successfully created."
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human) 
     else
       render :new, status: :unprocessable_entity
     end
@@ -27,7 +28,7 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
-      redirect_to @report, notice: "Report was successfully updated."
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -35,7 +36,7 @@ class ReportsController < ApplicationController
 
   def destroy
     @report.destroy
-    redirect_to reports_url, notice: "Report was successfully destroyed."
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
