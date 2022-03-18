cfe6459b5d7d07a7d0d8cb79964390a86ac5d24a
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-03-18 23:31:30 +0900

    Add model test for comment

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user1 = users(:one)
+    @book1 = books(:one)
+    @report = reports(:one)
+    @comment_book1user1 = comments(:book1user1)
+    @comment_report1user1 = comments(:report1user1)
+  end
+
+  test "comment has comment" do
+    assert_equal "book1-user1", @comment_book1user1.comment
+  end
+
+  test "comment belongs to book" do
+    assert_equal @book1, @comment_book1user1.commentable
+  end
+
+  test "comment belongs to report" do
+    assert_equal @report1, @comment_report1user1.commentable
+  end
+
+  test "comment belongs to user" do
+    assert_equal @user1, @comment_report1user1.user
+  end
 end
